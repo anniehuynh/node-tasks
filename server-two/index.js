2ab0cc38e6cb46ec46a932c3ab2db561bbf2c7a7
anniehuynh <anniehuynh253@gmail.com>
2021-10-29 13:55:35 +0300

    first try with pug

---
+++ server-two/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const express = require('express');
+const app = express(); // having an express application to start listening to request
+const port = 3000;
+
+//first Installing pug and then need to tell the app to use the view engine  to be pug
+app.set('view engine','pug');
+//give the name of the folder public to serve the static content html css
+app.use(express.static('public'))
+
+//(link to line 8) provide route to (all) the file with pug extender and transfer it to html format
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+//anseer from the root of server
+app.get('/hello', (req, res)=> { //request and what the server will response
+    res.send('Hello World!'); // sending back the response with text Hello World
+  });
+
+
+//give response of cat.json in /catinfo
+app.get('/catinfo', (req, res) => {
+    const cat = {
+      name: 'Frank',
+      birthdate: '2010-12-25',
+      weight: 5,
+    };
+    res.json(cat);
+  });
+
+  //listen to request
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
