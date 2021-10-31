202d187180b2035da42d1a1a54ff27f63d463897
anniehuynh <anniehuynh253@gmail.com>
2021-10-31 15:50:48 +0200

    add html methos and route params, (requirement 5)

--- week2/app.js
+++ week2/app.js
@@ -2,14 +2,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
+//4 Add cat routes which respond to POST, PUT and DELETE methods
 app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.put('/cat', (req, res) => {
+  res.send('With this endpoint you can edit cats.')
+});
+
+app.post('/cat', (req, res) => {
+  res.send('With this endpoint you can add cats.')
+});
+
+app.delete('/cat', (req, res) => {
+  res.send('With this endpoint you can delete cats.')
+});
+
+//5 Route parameters
 app.get('/cat/:catId', (req, res) => {
   console.log('/cat route', req.params);
-  res.send(`From this endpoint you can get a specific cat from catId: ${req.params.catId}`);
+  res.send(`You reqested a cat whose id is ${req.params.catId}`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
