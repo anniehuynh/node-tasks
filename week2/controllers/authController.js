a9c2b355f64bbbbb6a99cf3f16c1f7f7b8f01d33
anniehuynh <anniehuynh253@gmail.com>
2021-11-16 13:55:30 +0200

    auth and passport

---
+++ week2/controllers/authController.js
@@ -0,0 +1,25 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const login = (req, res, next) => {
+    //TODO: add passport authenticate
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+        if (err || !user) {
+           next(err);
+           return;
+        }
+
+        req.login(user, {session: false}, (err) => {
+            next(err);
+            return;
+        });
+
+        const token = jwt.sign(user, 'ewrweokfwdfvljj');
+        return res.json({user, token});
+    })(req, res, next);
+};
+
+module.exports = {
+  login,
+};
