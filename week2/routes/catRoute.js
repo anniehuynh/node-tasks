b8c8f31898cd6e6b6ecb24e57774c95cd885e001
anniehuynh <anniehuynh253@gmail.com>
2021-11-23 20:38:33 +0200

    modified users done

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,52 +1,63 @@
-"use strict";
+'use strict';
 // catRoute
-const express = require("express");
-
-const { body } = require("express-validator");
-
-//third party middleware multer import
-const multer = require("multer");
+const express = require('express');
+const { body } = require('express-validator');
+const multer = require('multer');
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.includes("image")) {
+  if (file.mimetype.includes('image')) {
     cb(null, true);
   } else {
     cb(null, false);
   }
 };
-//set up multer to send file uploaded to upload/ or create if does not exist
-const upload = multer({ dest: "./uploads/", fileFilter });
+const upload = multer({ dest: './uploads/', fileFilter });
 const {
   cat_list_get,
   cat_get,
   cat_post,
   cat_delete,
   cat_update,
-} = require("../controllers/catController.js"); //import from catController
-const router = express.Router(); //Router is the object handle
+} = require('../controllers/catController');
+const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get(cat_list_get)
   .post(
-    upload.single("cat"),
-    body("name").isLength({ min: 1 }),
-    body("birthdate").isDate(),
-    body("weight").isNumeric().isLength({ min: 1 }),
+    upload.single('cat'),
+    body('name').notEmpty(),
+    body('birthdate').isDate(),
+    body('weight').isNumeric(),
     cat_post
-  )
- 
+  );
 
-router.route("/:catId")
-    .get(cat_get)
-    .put(cat_update)
-    .delete(cat_delete)
-    .put(
-      body("name").isLength({ min: 1 }),
-      body("birthdate").isDate(),
-      body("weight").isNumeric().isLength({ min: 1 }),
-      cat_update
-    );
+router
+  .route('/:catId')
+  .get(cat_get)
+  .delete(cat_delete)
+  .put(
+    body('name').notEmpty(),
+    body('birthdate').isDate(),
+    body('weight').isNumeric(),
+    cat_update
+  );
 
-//make router available for other files
+router.route('/').get(cat_list_get).post(
+  upload.single('cat'),
+  body('name').notEmpty(),
+  body('birthdate').isDate(),
+  body('weight').isNumeric(),
+  // #remove for ui3# body('owner').isNumeric(),
+  cat_post
+);
+
+router.route('/:catId').get(cat_get).delete(cat_delete).put(
+  body('name').notEmpty(),
+  body('birthdate').isDate(),
+  body('weight').isNumeric(),
+  // #remove for ui3# body('owner').isNumeric(),
+  cat_update
+);
+
 module.exports = router;
--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -2,39 +2,34 @@
 // userRoute
 const express = require('express');
 const { body } = require('express-validator');
-
 const {
-    user_list_get,
-    user_get,
-    user_post,
-    user_delete,
-    user_update,
-    checkToken,
-} = require('../controllers/userController.js'); //import from userController
-const router = express.Router(); //Router is the object handle
-//instead of /user, just / because it is already replaced by the router
+  user_list_get,
+  user_get,
+  user_post,
+  checkToken,
+} = require('../controllers/userController');
+const router = express.Router();
+
 router.get('/token', checkToken);
 
-router.route('/')
-    .get(user_list_get)
-    .post(
-        body('name').isLength({ min: 3 }),
-        body('email').isEmail(),
-        body('passwd').matches('(?=.*[A-Z]).{8,}'),
-        user_post
-        )
-    .put(
-        body('name').isLength({ min: 3 }),
-        body('email').isEmail(),
-        body('passwd').matches('(?=.*[A-Z]).{8,}'),
-        user_update);
+router.get('/', user_list_get);
 
-       
+router.get('/:userId', user_get);
 
+router.post(
+  '/',
+  body('name').isLength({ min: 3 }),
+  body('email').isEmail(),
+  body('passwd').matches('(?=.*[A-Z]).{8,}'),
+  user_post
+);
 
-router.route('/:userId')
-    .get(user_get)
-    .delete(user_delete);
+router.put('/', (req, res) => {
+  res.send('From this endpoint you can modify users.');
+});
 
-//make router available for other files
+router.delete('/', (req, res) => {
+  res.send('From this endpoint you can delete users.');
+});
+
 module.exports = router;
